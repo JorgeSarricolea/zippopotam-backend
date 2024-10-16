830b4eca612089dd03b8b1c7ae6ee8be722b4a77
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-10-16 01:14:41 -0600

    feat(server): add main endpoint for zip code API
        - Initialized Express server with JSON parsing middleware.
        - Added /api/v1/zipcodes endpoint to handle zip code requests.
        - Configured server to listen on a specified port (default: 8000).
        - Included log statement for server launch confirmation.

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { config } from "./config/config.ts";
+import zipCodeRoutes from "./routes/ZipCodeRoutes.ts";
 
 const app = express();
 
 app.use(express.json());
 
 // Endpoints
+app.use("/api/v1/zipcodes", zipCodeRoutes);
 
 // Launch server
 const PORT = config.port || 8000;
