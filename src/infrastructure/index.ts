32cdf526927c4bd5a07251245279a2d90f2476ba
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-10-16 04:22:21 -0600

    feat: install cors and enable CORS on server
        - Installed cors to handle cross-origin requests.
        - Configured CORS to allow requests from http://localhost:5173.
        - Updated server configuration to

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { config } from "./config/config.ts";
 import zipCodeRoutes from "./routes/ZipCodeRoutes.ts";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:5173" }));
 
 // Endpoints
 app.use("/api/v1/zipcodes", zipCodeRoutes);
